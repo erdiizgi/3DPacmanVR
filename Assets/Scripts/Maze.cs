2005f8cf8a9b67cd4b2c122f139d57825923dc0c
titan <titan@titanjebozi.cz>
2016-01-16 12:39:36 +0100

    generator added

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -8,11 +8,17 @@ public class Maze : MonoBehaviour {
 
     public string fileName;
 
-    private TextAsset mazeData;
+	public GameObject wallPrefab;
+
+	private float x;
+	private float z;
+
 
 	// Use this for initialization
 	void Start () {
-	    mazeData = Resources.Load(fileName) as TextAsset;
+		x = 0;
+		z = 0;
+		this.Load ();
 	}
 	
 	// Update is called once per frame
@@ -28,7 +34,7 @@ public class Maze : MonoBehaviour {
             string line;
             // Create a new StreamReader, tell it which file to read and what encoding the file
             // was saved as
-            StreamReader theReader = new StreamReader(this.mazeData.text);
+			StreamReader theReader = new StreamReader("Assets/Resources/" + fileName + ".txt", Encoding.Default);
             // Immediately clean up the reader after this block of code is done.
             // You generally use the "using" statement for potentially memory-intensive objects
             // instead of relying on garbage collection.
@@ -43,8 +49,24 @@ public class Maze : MonoBehaviour {
 
                     if (line != null)
                     {
-                        Debug.Log(line);
-                        Debug.Log(" ");
+						for(int i = 0 ; i < line.Length ; i++){
+							string cur = line.Substring(i, 1);
+
+							switch(cur){
+							case "#":
+								Instantiate(wallPrefab, new Vector3(x, 0, z), Quaternion.identity);
+
+								x = x + 2;
+								break;
+							case " ":
+								x = x + 2;
+								break;
+							}
+						}
+
+						z = z + 2;
+						x = 0;
+
                     }
                 }
                 while (line != null);
