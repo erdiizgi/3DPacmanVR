635d7bfbe2f9a79af3e3325a190b76c9431c4146
John Beak <john.beak@gmail.com>
2016-01-16 19:12:54 +0100

    added ghost script

---
+++ Assets/Scripts/GhostScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostScript : MonoBehaviour
+{
+    public float speed;
+
+    Vector3 forward;
+    Vector3[] forwards;
+    float rotation;
+    float[] rotations;
+
+    // Use this for initialization
+    void Start()
+    {
+        forward = new Vector3(1f, 0f, 0f);
+        forwards = new Vector3[] {
+            new Vector3(1f, 0f, 0f),
+            new Vector3(0f, 0f, -1f),
+            new Vector3(-1f, 0f, 0f),
+            new Vector3(0f, 0f, 1f),
+        };
+        rotation = 0f;
+        rotations = new float[] {
+            0f,
+            90f,
+            180f,
+            360f
+        };
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = transform.position + forward * speed;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.GetComponent<PillScript>() != null)
+            return;
+        Debug.Log("Enter " + collider.gameObject.name + " X: " + transform.position.x + " Z: " + transform.position.z);
+        float newRotation = rotation;
+        Vector3 newForward = forward;
+        while (newRotation == rotation)
+        {
+            int random = Random.Range(0, rotations.Length);
+            newRotation = rotations[random];
+            newForward = forwards[random];
+        }
+        rotation = newRotation;
+        forward = newForward;
+        transform.rotation = Quaternion.Euler(0f, rotation, 0f);
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        Debug.Log("Enter " + collider.gameObject.name + " X: " + transform.position.x + " Z: " + transform.position.z);
+    }
+}
