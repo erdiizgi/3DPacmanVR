e9c68984ea2159f4976892c0db42e204ecb23117
Pavel Halbich <halbich.p@hotmail.com>
2016-01-16 13:49:09 +0100

    movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float MoveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        var moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * MoveSpeed;
+        transform.Translate(moveVector);
+
+        transform.Rotate(Vector3.up, Input.GetAxis("Mouse X"));
+        transform.Rotate(Vector3.up, Input.GetAxis("RightStick"));
+    }
+}
