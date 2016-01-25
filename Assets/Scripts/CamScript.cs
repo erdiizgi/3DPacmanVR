a9fd0ed0b5058b0577caa7af93107081b426b601
titan <titan@titanjebozi.cz>
2016-01-25 11:01:19 +0100

    Camera Keeper for VR added

--- Assets/Scripts/CamScript.cs
+++ Assets/Scripts/CamScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.VR;
 
 public class CamScript : MonoBehaviour {
 
@@ -7,6 +8,7 @@ public class CamScript : MonoBehaviour {
 
 	public Vector3 translation;
 	public Vector3 rotation;
+	public GameObject cameraKeeper;
 
 	public float hRot;
     public float StickMultiplier;
@@ -18,6 +20,42 @@ public class CamScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			InputTracking.Recenter();	
+		}
+
+		if(Input.GetKey(KeyCode.Alpha1))
+		{
+			this.rotation.x -= 0.2f;
+		}
+
+		if(Input.GetKey(KeyCode.Alpha2))
+		{
+			this.rotation.x += 0.2f;	
+		}
+
+		if(Input.GetKey(KeyCode.Alpha3))
+		{
+			this.rotation.y -= 0.2f;	
+		}
+
+		if(Input.GetKey(KeyCode.Alpha4))
+		{
+			this.rotation.y += 0.2f;	
+		}
+
+		if(Input.GetKey(KeyCode.Alpha5))
+		{
+			this.rotation.z -= 0.2f;	
+		}
+
+		if(Input.GetKey(KeyCode.Alpha6))
+		{
+			this.rotation.z += 0.2f;	
+		}
+
 
 		if (character == null) {
 			character = GameObject.Find ("Character(Clone)");
@@ -28,13 +66,16 @@ public class CamScript : MonoBehaviour {
 
 		Vector3 target = Quaternion.AngleAxis(hRot, Vector3.up) * translation;
 
-		gameObject.transform.position = character.transform.position + target;
-		gameObject.transform.eulerAngles = rotation;
 
-		hRot += Input.GetAxis ("Mouse X") + Input.GetAxis("RightStick") * StickMultiplier;
+		gameObject.transform.position = character.transform.position + target;
+		this.cameraKeeper.transform.position = character.transform.position + target;
+		gameObject.transform.eulerAngles = rotation;
+		this.cameraKeeper.transform.eulerAngles = rotation;
+		//hRot += Input.GetAxis ("Mouse X") + Input.GetAxis("RightStick") * StickMultiplier;
 
 		gameObject.transform.LookAt (character.transform.position);
 		gameObject.transform.eulerAngles += rotation;
+		this.cameraKeeper.transform.eulerAngles += rotation;
 
 
 	}
